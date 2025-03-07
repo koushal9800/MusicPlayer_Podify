5a05ef829ea8f6a83a7718b7eec30156d2b7d011
koushal barick <koushal.barick@stockedge.com>
2025-03-07 07:58:10 +0530

    React query try to implemrnt

--- App.tsx
+++ App.tsx
@@ -13,16 +13,22 @@ import store from './src/store';
 import AppNavigator from './src/navigation';
 import AppContainer from './src/components/AppContainer';
 import { clearAsyncStorage } from './src/utils/asyncStorage';
+import {QueryClient, QueryClientProvider} from 'react-query'
+
+
+const queryClient = new QueryClient()
 
 const App = () => {
-  clearAsyncStorage().then(()=>{
-    console.log('loggedout')
-  })
+  // clearAsyncStorage().then(()=>{
+  //   console.log('loggedout')
+  // })
   return (
     <Provider store={store}>
+      <QueryClientProvider client={queryClient} >
       <AppContainer>
       <AppNavigator />
       </AppContainer>
+      </QueryClientProvider>
     </Provider>
   );
 };
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,12 +3,29 @@ import { View,StyleSheet, Pressable,Text, Button } from "react-native";
 import colors from "../utils/colors";
 import { useDispatch } from "react-redux";
 import { updateNotification } from "../store/notification";
+import { useQuery } from "react-query";
+import client from "../api/client";
+import catchAsyncError from "../api/catchError";
 
 
 interface Props {}
 
+const fetchLatest = async()=>{
+    const {data} = await client.get('/audios/latest')
+    return data.audios
+}
+
 const Home :React.FC<Props> = (props) =>{
     const dispatch = useDispatch()
+  const query =  useQuery(['latest-uploads'],{
+        queryFn:()=> fetchLatest(),
+        onError(err){
+            const errorMessage = catchAsyncError(err)
+            dispatch(updateNotification({message:errorMessage,type:'error'}))
+        }
+    })
+
+    console.log('query',query)
     return(
        <View>
         <Text>Home</Text>
